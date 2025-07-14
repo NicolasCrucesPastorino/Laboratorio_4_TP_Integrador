00fb512126226136e4550890c0ed76d06879c97a
eeelian8 <elianleguizamon@gmail.com>
2025-07-14 20:32:46 -0300

    Ajustes en el merge entre Nicolas y la rama Main

--- TPINT_GRUPO_14_LAB4/src/main/java/dao/ICuotaDao.java
+++ TPINT_GRUPO_14_LAB4/src/main/java/dao/ICuotaDao.java
@@ -1,11 +1,21 @@
 package dao;
 
-
+import java.util.List;
 
 import entidad.CuotaPrestamo;;
 
-public interface ICuotaDao {	
-	
+public interface ICuotaDao {
+
 	void CrearCuota(CuotaPrestamo cuotaPrestamo);
+
 	void CancelarCuota(CuotaPrestamo cuotaPrestamo);
+
+	//public List<CuotaPrestamo> listarporFecha(int mes, int año);
+
+	//public int contarCuotas(List<CuotaPrestamo> lista, String estado);
+
+	//public float montoTotal(List<CuotaPrestamo> lista, String estado);
+
+	//public float calcularGanacia(List<CuotaPrestamo> lista);
+
 }
--- TPINT_GRUPO_14_LAB4/src/main/java/negocio/CuotaNegocioImpl.java
+++ TPINT_GRUPO_14_LAB4/src/main/java/negocio/CuotaNegocioImpl.java
@@ -1,33 +1,33 @@
-package negocio;
-
-import java.util.List;
-
-import dao.ICuotaDao;
-import daoImpl.CuotaDaoImpl;
-import entidad.CuotaPrestamo;
-
-public class CuotaNegocioImpl implements ICuotaNegocio{
-	
-	private ICuotaDao cuotaDao = new CuotaDaoImpl();
-
-	@Override
-	public List<CuotaPrestamo> listarporFecha(int mes, int año) {
-		return cuotaDao.listarporFecha(mes, año);
-	}
-
-	@Override
-	public int contarCuotas(List<CuotaPrestamo> lista, String estado) {
-		return cuotaDao.contarCuotas(lista, estado);
-	}
-
-	@Override
-	public float montoTotal(List<CuotaPrestamo> lista, String estado) {
-		return cuotaDao.montoTotal(lista, estado);
-	}
-
-	@Override
-	public float calcularGanacia(List<CuotaPrestamo> lista) {
-		return cuotaDao.calcularGanacia(lista);
-	}
-
-}
+//package negocio;
+//
+//import java.util.List;
+//
+//import dao.ICuotaDao;
+//import daoImpl.CuotaDaoImpl;
+//import entidad.CuotaPrestamo;
+//
+//public class CuotaNegocioImpl implements ICuotaNegocio{
+//	
+//	private ICuotaDao cuotaDao = new CuotaDaoImpl();
+//
+//	@Override
+//	public List<CuotaPrestamo> listarporFecha(int mes, int año) {
+//		return cuotaDao.listarporFecha(mes, año);
+//	}
+//
+//	@Override
+//	public int contarCuotas(List<CuotaPrestamo> lista, String estado) {
+//		return cuotaDao.contarCuotas(lista, estado);
+//	}
+//
+//	@Override
+//	public float montoTotal(List<CuotaPrestamo> lista, String estado) {
+//		return cuotaDao.montoTotal(lista, estado);
+//	}
+//
+//	@Override
+//	public float calcularGanacia(List<CuotaPrestamo> lista) {
+//		return cuotaDao.calcularGanacia(lista);
+//	}
+//
+//}
